70b84b7c43827b47f764d0ad8360fbb09fbfc32a
KShiro616 <nicopmlp@gmail.com>
2024-01-16 17:59:35 -0300

    fixed styling and indentation

--- favs.js
+++ favs.js
@@ -4,19 +4,24 @@ import { $displayMovies } from "./Modules/Constants.js";
 
 function createCard(movie) {
   return `
-  <article class="card h-auto">
+  <article class="card h-auto justify-between overflow-hidden bg-black">
 
+  <div class="flex flex-col">
   <img src="https://moviestack.onrender.com/static/${movie.image}">
-  <h2>${movie.title}</h2>
-  <h3>${movie.tagline}</h3>
-  <p>${movie.overview}</p>
-  <a href="details.html?id=${movie.id}"> See more </a>
+  <h2 class="text-xl text-purple-800">${movie.title}</h2>
+  <h3 class="text-sl text-pink-700">${movie.tagline}</h3>
+  <p >${movie.overview}</p>
+  </div>
 
-  <svg data-clicked-card-movie-id="${movie.id}" class="svg-heart h-7 w-7 overflow-visible self-end">
+  <div class="flex justify-between">
+  <a class="p-1 bg-purple-400 border rounded-xl" href="details.html?id=${movie.id}"> See more </a>
+
+  <svg data-clicked-card-movie-id="${movie.id}" class="fav h-7 w-7 overflow-visible self-end">
   <g transform="translate(0 -1028.4)">
   <path d="m7 1031.4c-1.5355 0-3.0784 0.5-4.25 1.7-2.3431 2.4-2.2788 6.1 0 8.5l9.25 9.8 9.25-9.8c2.279-2.4 2.343-6.1 0-8.5-2.343-2.3-6.157-2.3-8.5 0l-0.75 0.8-0.75-0.8c-1.172-1.2-2.7145-1.7-4.25-1.7z" fill="#c0392b"/>
   </g>
   </svg>
+  </div>
 
   </article>
   `
@@ -76,7 +81,7 @@ $displayMovies.addEventListener("click", (event) => {
   const clickedOn = event.target;
   const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
 
-  if (clickedOn.classList.contains("svg-heart")) {
+  if (clickedOn.classList.contains("fav")) {
 
     const movieId = clickedOn.dataset.clickedCardMovieId;
     const updateFavorites = favorites.filter(id => id !== movieId)
--- main.js
+++ main.js
@@ -15,37 +15,47 @@ function createCard(movie, isFav) {
 
   if (isFav) {
     return `
-    <article class="card">
-  
-    <img src="https://moviestack.onrender.com/static/${movie.image}">
-    <h2>${movie.title}</h2>
-    <h3>${movie.tagline}</h3>
-    <p>${movie.overview}</p>
-    <a href="details.html?id=${movie.id}"> See more </a>
+    <article class="card h-auto justify-between overflow-hidden bg-black">
 
-    <svg data-clicked-card-movie-id="${movie.id}" class="notFav h-7 w-7 overflow-visible self-end">
+    <div class="flex flex-col">
+    <img src="https://moviestack.onrender.com/static/${movie.image}">
+    <h2 class="text-xl text-purple-800">${movie.title}</h2>
+    <h3 class="text-sl text-pink-700">${movie.tagline}</h3>
+    <p >${movie.overview}</p>
+    </div>
+  
+    <div class="flex justify-between">
+    <a class="p-1 bg-purple-400 border rounded-xl" href="details.html?id=${movie.id}"> See more </a>
+  
+    <svg data-clicked-card-movie-id="${movie.id}" class="fav h-7 w-7 overflow-visible self-end">
     <g transform="translate(0 -1028.4)">
-    <path class="z-index-0" d="m7 1031.4c-1.5355 0-3.0784 0.5-4.25 1.7-2.3431 2.4-2.2788 6.1 0 8.5l9.25 9.8 9.25-9.8c2.279-2.4 2.343-6.1 0-8.5-2.343-2.3-6.157-2.3-8.5 0l-0.75 0.8-0.75-0.8c-1.172-1.2-2.7145-1.7-4.25-1.7z" fill="#c0392b"/>
-  </g>
+    <path d="m7 1031.4c-1.5355 0-3.0784 0.5-4.25 1.7-2.3431 2.4-2.2788 6.1 0 8.5l9.25 9.8 9.25-9.8c2.279-2.4 2.343-6.1 0-8.5-2.343-2.3-6.157-2.3-8.5 0l-0.75 0.8-0.75-0.8c-1.172-1.2-2.7145-1.7-4.25-1.7z" fill="#c0392b"/>
+    </g>
     </svg>
+    </div>
   
     </article>
     `
   }
   return `
-  <article class="card">
+  <article class="card h-auto justify-between overflow-hidden bg-black">
 
+  <div class="flex flex-col">
   <img src="https://moviestack.onrender.com/static/${movie.image}">
-  <h2>${movie.title}</h2>
-  <h3>${movie.tagline}</h3>
-  <p>${movie.overview}</p>
-  <a href="details.html?id=${movie.id}"> See more</a>
+  <h2 class="text-xl text-purple-800">${movie.title}</h2>
+  <h3 class="text-sl text-pink-700">${movie.tagline}</h3>
+  <p >${movie.overview}</p>
+  </div>
+
+  <div class="flex justify-between">
+  <a class="p-1 bg-purple-400 border rounded-xl" href="details.html?id=${movie.id}"> See more </a>
 
   <svg data-clicked-card-movie-id="${movie.id}" class="notFav h-7 w-7 overflow-visible self-end">
   <g transform="translate(0 -1028.4)">
-    <path d="m7 1031.4c-1.5355 0-3.0784 0.5-4.25 1.7-2.3431 2.4-2.2788 6.1 0 8.5l9.25 9.8 9.25-9.8c2.279-2.4 2.343-6.1 0-8.5-2.343-2.3-6.157-2.3-8.5 0l-0.75 0.8-0.75-0.8c-1.172-1.2-2.7145-1.7-4.25-1.7z" stroke="#000000" fill="none" stroke-width="1"/>
-  </g>
+  <path d="m7 1031.4c-1.5355 0-3.0784 0.5-4.25 1.7-2.3431 2.4-2.2788 6.1 0 8.5l9.25 9.8 9.25-9.8c2.279-2.4 2.343-6.1 0-8.5-2.343-2.3-6.157-2.3-8.5 0l-0.75 0.8-0.75-0.8c-1.172-1.2-2.7145-1.7-4.25-1.7z" stroke="#c0392b" fill="none" stroke-width="1"/>
+</g>
   </svg>
+  </div>
 
   </article>
   `
@@ -178,7 +188,7 @@ function removeFromFavorites(movieId, clickedOn) {
 
   clickedOn.innerHTML = `
   <g transform="translate(0 -1028.4)">
-  <path d="m7 1031.4c-1.5355 0-3.0784 0.5-4.25 1.7-2.3431 2.4-2.2788 6.1 0 8.5l9.25 9.8 9.25-9.8c2.279-2.4 2.343-6.1 0-8.5-2.343-2.3-6.157-2.3-8.5 0l-0.75 0.8-0.75-0.8c-1.172-1.2-2.7145-1.7-4.25-1.7z" stroke="#000000" fill="none" stroke-width="1"/>
+  <path d="m7 1031.4c-1.5355 0-3.0784 0.5-4.25 1.7-2.3431 2.4-2.2788 6.1 0 8.5l9.25 9.8 9.25-9.8c2.279-2.4 2.343-6.1 0-8.5-2.343-2.3-6.157-2.3-8.5 0l-0.75 0.8-0.75-0.8c-1.172-1.2-2.7145-1.7-4.25-1.7z" stroke="#c0392b" fill="none" stroke-width="1"/>
 </g>
   `
   return localStorage.setItem("favorites", JSON.stringify(updateFavorites))
